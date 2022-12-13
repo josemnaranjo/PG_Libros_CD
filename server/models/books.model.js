d1f70d3def323ac33272a1e3c2652631d83bc843
Josemaria Naranjo <josemnaranjoc@gmail.com>
2022-12-12 22:55:11 -0300

    first commit

---
+++ server/models/books.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const BookSchema = new mongoose.Schema({
+    title: {
+        type:String,
+        default:"",
+        required:[true,"El titulo del libro es obligatorio"]
+    },
+    author: {
+        type:String,
+        default:"",
+        required:[true,"El autor del libro es obligatorio"]
+    },
+    genre:{
+        type:String,
+        default:"",
+        required:[true,"Debes ingresar el género del libro"]
+    },
+    summary:{
+        type:String,
+        default:"",
+        required:[true,"Deber ingresar un breve resumen del libro"]
+    },
+    creatorId:{
+        type:String,
+        required:[true,"Id de creador es obligatorio"]
+    },
+    interestId:{
+        type:String,
+    }
+},{timestamps:true});
+
+
+const Book = mongoose.model("Book",BookSchema);
+module.exports = {BookSchema,Book}
