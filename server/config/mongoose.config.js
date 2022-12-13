2fb8274d661322eb3fe7aeccf5ad26e5ef485c64
Josemaria Naranjo <josemnaranjoc@gmail.com>
2022-12-13 11:03:55 -0300

    Confirmacion

--- server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.DB_LINK , {
-    useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-    .then(()=>console.log("CONEXION CON BASE DE DATOS ESTABLECIDA"))
-    .catch(err=> console.log("PROBLEMAS CON CONECTARSE A LA BASE DE DATOS ",err))
